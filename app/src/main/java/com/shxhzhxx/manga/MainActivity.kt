6f9b9cdc7da4afb07ce84b00d31cae32cc7eef3a
SH <sh.ylzhxx@163.com>
2018-10-11 12:12:20 +0800

    use architecture component

--- app/src/main/java/com/shxhzhxx/manga/MainActivity.kt
+++ app/src/main/java/com/shxhzhxx/manga/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.shxhzhxx.manga
 
 import android.Manifest
+import android.app.Application
 import android.graphics.Color
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.*
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.shxhzhxx.imageloader.ImageLoader
@@ -16,7 +20,7 @@ import kotlinx.android.synthetic.main.directory.view.*
 import java.util.*
 import kotlin.collections.LinkedHashMap
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), Observer<List<Path>> {
     private val mAdapter: MyAdapter = MyAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,70 +33,87 @@ class MainActivity : BaseActivity() {
 
         performRequestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), object : PermissionsResultListener() {
             override fun onPermissionGranted() {
-                Thread {
-                    val map: LinkedHashMap<String, Path> = LinkedHashMap()
-                    contentResolver.query(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            arrayOf(MediaStore.Images.Media.DATA),
-                            "${MediaStore.Images.Media.MIME_TYPE}=? or ${MediaStore.Images.Media.MIME_TYPE}=?",
-                            arrayOf("image/jpeg", "image/png"),
-                            MediaStore.Images.Media.DATA
-                    )?.apply {
-                        while (moveToNext()) {
-                            getString(getColumnIndex(MediaStore.Images.Thumbnails.DATA)).let { data ->
-                                data.dropLastWhile {
-                                    it != '/'
-                                }.dropLast(1).let { path ->
-                                    if (path !in map) {
-                                        map[path] = Path(path.takeLastWhile {
-                                            it != '/'
-                                        }, path, data)
-                                    }
-                                    map[path]?.apply {
-                                        count += 1
-                                    }
-                                }
-                            }
-                        }
-                        close()
-                    }
-                    runOnUiThread {
-                        if (!isDestroyed) {
-                            mAdapter.list = map.values.toList()
-                            mAdapter.notifyDataSetChanged()
-                        }
-                    }
-                }.start()
+                this@MainActivity.apply {
+                    val viewModel = ViewModelProviders.of(this).get(PathViewModel::class.java)
+                    viewModel.getPaths().observe(this, this)
+                }
             }
         })
     }
 
-    data class Path(val displayName: String, val path: String, val cover: String, var count: Int = 0)
+    override fun onChanged(list: List<Path>) {
+        mAdapter.setList(list)
+    }
 
     inner class MyAdapter : RecyclerView.Adapter<MyAdapter.MyHolder>() {
-        var list: List<Path> = ArrayList()
+        private var mList: List<Path> = ArrayList()
+        fun setList(list: List<Path>) {
+            mList = list
+            notifyDataSetChanged()
+        }
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
             return MyHolder(LayoutInflater.from(parent.context).inflate(R.layout.directory, parent, false))
         }
 
         override fun getItemCount(): Int {
-            return list.size
+            return mList.size
         }
 
         override fun onBindViewHolder(holder: MyHolder, position: Int) {
-            holder.itemView.displayName.text = list[position].displayName
-            holder.itemView.count.text = list[position].count.toString()
-            ImageLoader.getInstance().load(list[position].cover).waitMeasure().centerCrop().tag(IDENTIFY).into(holder.itemView.preview)
+            holder.itemView.displayName.text = mList[position].displayName
+            holder.itemView.count.text = mList[position].count.toString()
+            ImageLoader.getInstance().load(mList[position].cover).waitMeasure().centerCrop().tag(IDENTIFY).into(holder.itemView.preview)
         }
 
         inner class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
             override fun onClick(v: View?) {
-                launchGalleryActivity(applicationContext, list[adapterPosition].path)
+                launchGalleryActivity(applicationContext, mList[adapterPosition].path)
             }
 
             init {
                 itemView.setOnClickListener(this)
             }
         }
+    }
+}
+
+data class Path(val displayName: String, val path: String, val cover: String, var count: Int = 0)
+
+class PathViewModel(application: Application) : AndroidViewModel(application) {
+    private val paths = MutableLiveData<List<Path>>()
+
+    init {
+        Thread {
+            application.contentResolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(MediaStore.Images.Media.DATA),
+                    "${MediaStore.Images.Media.MIME_TYPE}=? or ${MediaStore.Images.Media.MIME_TYPE}=?",
+                    arrayOf("image/jpeg", "image/png"),
+                    MediaStore.Images.Media.DATA
+            )?.apply {
+                val map: LinkedHashMap<String, Path> = LinkedHashMap()
+                while (moveToNext()) {
+                    getString(getColumnIndex(MediaStore.Images.Thumbnails.DATA)).let { data ->
+                        data.dropLastWhile {
+                            it != '/'
+                        }.dropLast(1).let { path ->
+                            if (path !in map) {
+                                map[path] = Path(path.takeLastWhile {
+                                    it != '/'
+                                }, path, data)
+                            }
+                            map[path]!!.count += 1
+                        }
+                    }
+                }
+                close()
+                paths.postValue(map.values.toList())
+            }
+        }.start()
+    }
+
+    fun getPaths(): LiveData<List<Path>> {
+        return paths
     }
 }
